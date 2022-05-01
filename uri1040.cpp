45ab94edc4c4b02046ec5e835b523cf709b0babd
Maurício Nascimento Cunha <mauricio.nascimento.cunha@gmail.com>
2022-05-01 15:26:43 -0300

    Merge branch 'master' of https://github.com/Mauriciocunha1996/URI-Online-Judge-Solutions

---
+++ uri1040.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    double n1 , n2, n3, n4, media;
+
+    cin>> n1 >> n2 >> n3 >>n4;
+
+    media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1))/10;
+    cout.precision(1);
+    cout << "Media: " << fixed << media << endl;
+
+    if(media >= 7){
+        cout<<"Aluno aprovado."<<endl;
+
+    }else if(media >= 5 && media < 7){
+        cout << "Aluno em exame." << endl;
+        cin>>n1;
+        cout << "Nota do exame: " << fixed << n1 <<endl;
+        media= (media + n1)/2;
+
+        if(media >= 5){
+            cout<<"Aluno aprovado."<<endl;
+        }else {
+            cout<<"Aluno reprovado."<<endl;
+            
+        }
+        cout << "Media final: " << fixed << media << endl;
+    }else{
+        cout << "Aluno reprovado."<<endl;
+    }
+
+    return 0;
+
+}
