10d9993e8b949dc83b0e2b1a7095f89b9b072445
Maurício Nascimento Cunha <mauricio.nascimento.cunha@gmail.com>
2022-05-04 17:05:35 -0300

    solution in c++

--- uri1074.cpp
+++ uri1074.cpp
@@ -6,18 +6,20 @@ int main(){
     int n;
 
     cin>>n;
+    
+    int v[n];
 
     for(int i = 0; i < n; i++){
-        cin>>i;
-        if(i>0 && i%2==0){
+        cin>>v[i];
+        if(v[i]>0 && v[i] % 2 == 0){
             cout<<"EVEN POSITIVE"<<endl;
-        }else if(i<0 && i%2==0){
+        }else if(v[i]<0 && v[i] % 2 == 0){
             cout<<"EVEN NEGATIVE"<<endl;
-        }else if(i>0 && i%2!=0){
+        }else if(v[i]>0 && v[i] % 2 != 0){
             cout<<"ODD POSITIVE"<<endl;
-        }else if(i<0 && i%2!=0){
+        }else if(v[i] < 0 && v[i] % 2 != 0){
             cout<<"ODD NEGATIVE"<<endl;
-        }else if(i==0){
+        }else if(v[i]==0){
             cout<<"NULL"<<endl;
         }
     }
